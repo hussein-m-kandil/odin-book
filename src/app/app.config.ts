fdc2f88943b7b33d1a239362da35477a5e1c68dd
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2026-02-15 08:46:48 +0200

    Add ColorScheme service to control global theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { initColorScheme } from './color-scheme';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding()),
+    provideAppInitializer(initColorScheme),
+    provideBrowserGlobalErrorListeners(),
   ],
 };
