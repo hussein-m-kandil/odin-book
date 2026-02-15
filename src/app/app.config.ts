41dc95febd7c4a53cab1746f5b19be4fd24104ab
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2026-02-15 09:08:48 +0200

    Add interceptor to retry some failed requests

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,14 +4,16 @@ import {
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { initColorScheme, DARK_SCHEME_CN } from './color-scheme';
+import { retryingInterceptor } from './retrying-interceptor';
 import { default as Aura } from '@primeuix/themes/aura';
 import { providePrimeNG } from 'primeng/config';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withInterceptors([retryingInterceptor])),
     provideRouter(routes, withComponentInputBinding()),
     provideAppInitializer(initColorScheme),
     provideBrowserGlobalErrorListeners(),
