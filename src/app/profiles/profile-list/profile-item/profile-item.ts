d7b1f543e4d77860d5bb206e9b294c6fcc83014a
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2026-02-19 21:14:55 +0200

    Edit ProfileItem styles

--- src/app/profiles/profile-list/profile-item/profile-item.ts
+++ src/app/profiles/profile-list/profile-item/profile-item.ts
@@ -1,13 +1,14 @@
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { FollowToggle } from '../../profile/follow-toggle';
 import { Component, input } from '@angular/core';
+import { ButtonDirective } from 'primeng/button';
 import { Avatar } from '../../profile/avatar';
 import { Profile } from '../../../app.types';
 import { Ripple } from 'primeng/ripple';
 
 @Component({
   selector: 'app-profile-item',
-  imports: [RouterLinkActive, RouterLink, FollowToggle, Ripple, Avatar],
+  imports: [ButtonDirective, RouterLinkActive, RouterLink, FollowToggle, Ripple, Avatar],
   templateUrl: './profile-item.html',
   styles: ``,
 })
